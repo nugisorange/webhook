c615b23821268a8d097c3f1221ec5fa848410318
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-11-12 16:19:44 +0700

    updated

---
+++ type.go
@@ -0,0 +1,15 @@
+package webhook
+
+type Reply struct {
+	Message string `bson:"messsage"`
+}
+
+var stringreply = []string{
+	"IHHH jangan SPAAM berisik tau faibit lagi tidur",
+	"Kamu tampan tau",
+	"Ihhh kamu cantik sekali banget",
+	"bray, lebih baik beliin aku nasgor",
+	"Jangan galak galak dong kak, aku catut tauu",
+	"Mawar Indah hanya akan muncul dipagi hari, MAKANYA BANGUN PAGI KAK",
+	"kuykuy hari ini faibit bahagiaaa banget",
+}
